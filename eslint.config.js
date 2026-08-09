35388d14b08b99130d91a7698213f657544d9f3a
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-08-09 06:46:33 +0200

    Replace Electron remote usage

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ const prettier = require("eslint-config-prettier");
 
 // Modules provided by the Lumine/Electron runtime rather than this package's own
 // manifest, so they aren't resolvable by eslint-plugin-n.
-const runtimeModules = ["atom", "electron", "@electron/remote"];
+const runtimeModules = ["atom", "electron"];
 
 module.exports = [
   js.configs.recommended,
